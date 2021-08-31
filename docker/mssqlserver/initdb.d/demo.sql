9fd475f80382c2eb3824f9d0f71b4bf251e23262
hideru <jota.16.stigma.world@gmail.com>
2021-08-31 18:06:13 +0900

    Added .sql

---
+++ docker/mssqlserver/initdb.d/demo.sql
@@ -0,0 +1,8 @@
+﻿CREATE DATABASE DemoDB;
+GO
+USE DemoDB;
+GO
+CREATE TABLE Members (ID int, Name varchar(255), Age int, FireDate DateTime);
+GO
+insert into Members (ID, Name, Age, FireDate) values (1, "StoneSwamp", 58, "2019-01-01T00:00:00");
+GO
